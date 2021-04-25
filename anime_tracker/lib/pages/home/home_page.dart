f6d4a062b76803b1efbe278934bf4cf46c3f4cb3
Alpii <alparslanabdik@gmail.com>
2021-04-25 02:43:29 +0200

    search bar done

--- anime_tracker/lib/pages/home/home_page.dart
+++ anime_tracker/lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:anime_tracker/utility/app_colors.dart';
+import 'package:anime_tracker/utility/widget/costume_appbar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +12,51 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: AppColors.scaffoldBg,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: CostumeAppBar(),
+          body: Container(
+            //Search bar
+            child: Column(
+              children: [
+                _searchTextEditWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _searchTextEditWidget() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.black.withOpacity(0.3),
+            offset: Offset(0, 0),
+            blurRadius: 5,
+            spreadRadius: 1,
+          )
+        ],
+      ),
+      child: TextField(
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        decoration: InputDecoration(
+            hintText: "Search",
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            border: InputBorder.none,
+            suffixIcon: Icon(
+              Icons.search,
+              size: 30,
+            )),
+      ),
+    );
   }
 }
--- anime_tracker/lib/utility/widget/costume_appbar.dart
+++ anime_tracker/lib/utility/widget/costume_appbar.dart
@@ -1,10 +1,38 @@
+import 'package:anime_tracker/utility/app_assets.dart';
 import 'package:flutter/material.dart';
 
-class CostumeAppBar extends StatelessWidget {
+class CostumeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CostumeAppBar({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //AppIcon
+          Icon(
+            Icons.menu,
+            size: 40,
+          ),
+          Text(
+            "Cashier Basket",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          //User Image
+          SizedBox(
+            height: 60,
+            width: 60,
+            child: CircleAvatar(
+                backgroundImage: Image.asset(AppAssets.heartIcon).image),
+          )
+        ],
+      ),
+    );
   }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(60);
 }
